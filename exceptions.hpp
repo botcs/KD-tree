b780ff93a7c95eaa817ab809c052e59768b2a2da
botcs <botcs@users.noreply.github.com>
2015-10-31 17:30:27 +0100

    Create exceptions.hpp

---
+++ exceptions.hpp
@@ -0,0 +1,48 @@
+#ifndef EXCEPTIONS_HPP_INCLUDED
+#define EXCEPTIONS_HPP_INCLUDED
+#include <exception>
+
+//LANG: HUN
+
+class internal_error : public std::exception
+{
+	const char *message;
+
+public:
+	internal_error(const char *msg = "Hiba van az implementacioban!") : message(msg) {}
+
+	const char *what() const throw() { return message; }
+};
+
+class invalid_binary_search_tree : public std::exception
+{
+public:
+	const char *what() const throw() { return "Serulnek a binaris keresofa kriteriumok!"; }
+};
+
+class invalid_dimension : public std::exception
+{
+public:
+	const char *what() const throw() { return "Rossz kulcs dimenzio!"; }
+};
+
+class invalid_iterator : public std::exception
+{
+public:
+    const char *what() const throw() { return "NULL elemre mutat az iterator!"; }
+};
+
+class empty_tree : public std::exception
+{
+public:
+    const char *what() const throw() { return "Ures a fa, nem ertelmezheto a kereses!"; }
+};
+
+class copy_not_implemented : public std::exception
+{
+public:
+    const char *what() const throw() { return "Masolas nincs implementalva!"; }
+};
+
+#endif // EXCEPTIONS_HPP_INCLUDED
+
